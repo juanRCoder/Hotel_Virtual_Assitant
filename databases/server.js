8c6f2b4b4d8cb8edab1d77315550fee910bd0f80
Jharanza <51296177+Jharanza@users.noreply.github.com>
2023-12-08 22:52:01 -0500

    feat: Add the conexión to the database

---
+++ databases/server.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+//Connect to MondoDB
+mongoose.connect('mongodb://127.0.0.1/testaroo');
+
+mongoose.connection.once('open', function() {
+    console.log("connection has been made...");
+}).on('error', function(error) {
+    console.log("Connection error: ", error);
+});
