fb016c270affa1a15dee631d533780c8c82aa211
Jharanza <51296177+Jharanza@users.noreply.github.com>
2023-12-12 18:13:13 -0500

    feat: I encapsulated the code for establishing a connection in a separate module

--- databases/database.js
+++ databases/database.js
@@ -3,10 +3,12 @@ import mongoose from 'mongoose';
 //Connect to MondoDB
 mongoose.connect('mongodb://127.0.0.1/testaroo');
 
-mongoose.connection.once('open', function() {
+const connection = mongoose.connection;
+
+connection.once('open', function() {
     console.log("connection has been made...");
 }).on('error', function(error) {
     console.log("Connection error: ", error);
 });
 
-export default mongoose.connection;
+export default connection;
