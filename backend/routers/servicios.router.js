04f7eafd9f83a411b5e090a3761e9475df8917fe
juanRCoder <guillecarck@gmail.com>
2023-12-18 22:23:55 -0500

    feat: add new route get for extract the data from the service

--- backend/routers/servicios.router.js
+++ backend/routers/servicios.router.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { postServicio} from "../controllers/servicios.controller.js";
+import {
+  postServicio,
+  getServicio,
+} from "../controllers/servicios.controller.js";
 
 const servicioRouter = express.Router();
 
-// Api post = verifica pide un servicio o no
-servicioRouter.post("/dashboard/servicios/enviarServicio", postServicio);
+servicioRouter.post("/enviarServicio/:id", postServicio);
+servicioRouter.get("/extraerServicio/:idService", getServicio);
 
 export { servicioRouter };
