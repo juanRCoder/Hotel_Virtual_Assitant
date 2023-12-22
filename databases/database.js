50058f7fd662d4b14dafbf63829fadfee8325338
Jhony Arana <jharanza1986@gmail.com>
2023-12-22 10:24:36 -0500

    fix: Delete the message of the connection to fix error

--- databases/database.js
+++ databases/database.js
@@ -1,27 +1,13 @@
-import mongoose  from 'mongoose';
+import mongoose from 'mongoose';
 
 //Connect to MondoDB
 const url = 'mongodb://127.0.0.1/testaroo';
 
 async function connectDB() {
     try {
-        await mongoose.connect(url, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(url)
+        console.log("connection has been made...");
 
-        mongoose.connection.on('connected', () => {
-            console.log("connection has been made...");
-        });
-
-        mongoose.connection.on('disconnected', () => {
-            console.log('connection closed');
-        });
-
-        mongoose.connection.on('error', (error) => {
-            console.error('connection error: ', error);
-        });
-        
     } catch (err) {
         console.log("Connection error: ", err)
     }
