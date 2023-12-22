23f282ac691de67311596f52a3b7e059a6563172
juanRCoder <guillecarck@gmail.com>
2023-12-22 11:16:19 -0500

    feat: modify routers for preguntas, tecnico

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Login from '../pages/Login';
+// import Login from '../pages/Login';
 
 import Layout from '../Layout/Layout';
 import NotFound from '../pages/NotFound';
@@ -16,11 +16,11 @@ const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />} />
-      <Route path="/Login" element={<Login />} />
+      {/* <Route path="/Login" element={<Login />} /> */}
       <Route path="/Dashboard/:id" element={<Dashboard />} />
       <Route path="/dashboard/servicios/:id" element={<Servicios />} />
-      <Route path="/dashboard/preguntas" element={<Preguntas />} />
-      <Route path="/dashboard/tecnico" element={<Tecnico />} />
+      <Route path="/dashboard/preguntas/:id" element={<Preguntas />} />
+      <Route path="/dashboard/tecnico/:id" element={<Tecnico />} />
       <Route path="/dashboard/restaurante" element={<Restaruante />} />
       <Route path="/*" element={<NotFound />} />
 
