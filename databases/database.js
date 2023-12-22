8be4c95d91a1385cef22f800c2a4e0bcc63aace5
Jhony Arana <jharanza1986@gmail.com>
2023-12-22 10:19:06 -0500

    feat: Add messages of connection state

--- databases/database.js
+++ databases/database.js
@@ -1,12 +1,27 @@
-import mongoose from 'mongoose';
+import mongoose  from 'mongoose';
 
 //Connect to MondoDB
 const url = 'mongodb://127.0.0.1/testaroo';
 
 async function connectDB() {
     try {
-        await mongoose.connect(url)
-        console.log("connection has been made...");
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        mongoose.connection.on('connected', () => {
+            console.log("connection has been made...");
+        });
+
+        mongoose.connection.on('disconnected', () => {
+            console.log('connection closed');
+        });
+
+        mongoose.connection.on('error', (error) => {
+            console.error('connection error: ', error);
+        });
+        
     } catch (err) {
         console.log("Connection error: ", err)
     }
