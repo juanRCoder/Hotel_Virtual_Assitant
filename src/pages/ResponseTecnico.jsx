ca857c75475dad970a5dfeb98798d49b75fd88c4
oskar <omoralespj@gmail.com>
2023-12-22 21:42:33 -0500

    update file response

--- src/pages/ResponseTecnico.jsx
+++ src/pages/ResponseTecnico.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import moment from "moment-timezone";
+import Routers from '../routes/Routers';
 
 function ResponseTecnico() {
   const { idServiceTec } = useParams();
