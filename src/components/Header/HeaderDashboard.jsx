910d0bf47086cf1113c67d4665e07e3cd2f5ce6f
oskar <omoralespj@gmail.com>
2023-12-22 21:46:17 -0500

    fix error to merge

--- src/components/Header/HeaderDashboard.jsx
+++ src/components/Header/HeaderDashboard.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo-removebg-preview.png';
-import Lottie from "lottie-react";
-import animation from "../../assets/back.json";
+import Lottie from 'lottie-react';
+import animation from '../../assets/back.json';
 
-
-
-function HeaderDashboard() {
-  
+function HeaderDashboard({ serviceTecnico }) {
   return (
-    
     <div className="flex flex-col items-center relative">
-      <div className="absolute  top-0 left-0 w-20 h-1 mt-5 ">
+      <div className="absolute top-0 left-0 w-20 h-1 mt-5">
         <img className="" src={logo} alt="LOGO" />
       </div>
+
       <div>
-        
-        <Lottie className="w-12 absolute top-4 right-6" animationData={animation} />
+        <Link to={`/dashboard/${serviceTecnico && serviceTecnico.Cliente.id}`}>
+          <button>
+            <Lottie className="w-12 absolute top-4 right-6" animationData={animation} />
+          </button>
+        </Link>
       </div>
-      <div className="flex items-center justify-center flex-col">
+
+      {/* <div className="flex items-center justify-center flex-col">
         <h1 className="font-Oswald text-7xl text-center pt-5">
           HOTEL OTELERO
         </h1>
-      </div>
+      </div> */}
     </div>
   );
 }
