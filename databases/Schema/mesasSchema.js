150717185a51b6d4e646212774a0f3902b687489
Jharanza <51296177+Jharanza@users.noreply.github.com>
2023-12-26 17:06:53 -0500

    fix: fixing typo in mesasSchema

--- databases/Schema/mesasSchema.js
+++ databases/Schema/mesasSchema.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
-const MesasSchema = new mongoose.Schema({
-    tipo_mesa: String,
-    cantidad_sillas: Number,
+const mesaSchema = new mongoose.Schema({
+    tipoMesa: String,
+    cantidadSillas: Number,
     estado: Boolean
 });
 
-const Mesas = mongoose.Model('Mesas', MesasSchema);
+const Mesas = mongoose.model('Mesas', mesaSchema);
 
 export default Mesas;
