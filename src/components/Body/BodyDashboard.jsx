3a9d1647ddc69f5ef14ab918ab0c2af4cea573f8
oskar <omoralespj@gmail.com>
2023-12-27 10:27:55 -0500

    fix id, to link

--- src/components/Body/BodyDashboard.jsx
+++ src/components/Body/BodyDashboard.jsx
@@ -121,7 +121,7 @@ function BodyDashboard() {
           </button>
         </Link>
 
-        <Link to="/dashboard/restaurante/:id">
+        <Link to={`/dashboard/tecnico/${id}`}>
           <button
             className="
             font-semibold 
