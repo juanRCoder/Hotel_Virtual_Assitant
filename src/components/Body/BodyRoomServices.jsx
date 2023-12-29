11733af4ba2c667f41ae5c483cabd0c539a2a111
Jharanza <51296177+Jharanza@users.noreply.github.com>
2023-12-29 17:10:17 -0500

    fix: Fixing the conexion to the database

--- src/components/Body/BodyRoomServices.jsx
+++ src/components/Body/BodyRoomServices.jsx
@@ -2,33 +2,33 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom"; 
 
 const BodyRoomServices = () => {
-    const [formData, setFormData ] = useState({
-        fecha: '',
-        hora: 0,
-        minutes: 0,
-        bebidas: '',
-        menu: '',
-        cantidadBebidas: 0,
-        cantidadMenu: 0,
-        resumen: ''
-    });
+
+  const fechaInicial = new Date();
+  const formattedDate = fechaInicial.toISOString().split("T")[0];
+  
+  const [formData, setFormData ] = useState({
+    bebidaOption: "",
+    menuOption: "",
+    cant_beb: 0,
+    cant_menu: 0,
+    otros_detalles: "",
+    fecha: formattedDate,
+    hora: 0,
+    minutes: 0
+  });
 
     const [bebidasOptions, setBebidasOptions] = useState([]);
-    const [menuOptions, setMenuOptions] = useState([]);
+    const [menuOptions, setMenuOptions] = useState(null);
     const { id } = useParams();
-    const clienteId = id;
-    console.log('clienteId: ', clienteId)
-    console.log('ID: ',id);
+
     useEffect(() => {
         // Función para obtener informaicón de las bebidas y el menú
         const fetchData = async () => {
             try {
-                const bebidasResponse = await fetch(`http://localhost:3004/extraerBebidas/`);
-
-                const menuResponse = await fetch('http://localhost:3004/extraerMenu/');
+                const bebidasResponse = await fetch("/extraerBebidas");
+                const menuResponse = await fetch("/extraerMenu");
 
                 const bebidasData = await bebidasResponse.json();
-
                 const menuData = await menuResponse.json();
 
                 setBebidasOptions(bebidasData);
@@ -38,47 +38,33 @@ const BodyRoomServices = () => {
             };
         };
         fetchData();
-
     }, []);
 
     const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [ name ]: value })
+      setFormData({
+        ...formData,
+        [e.target.name]: e.target.value,
+      });
     };
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log('Datos enviados', formData);
-        try {
-          const response = await fetch(`http://localhost:3004/dashboard/enviarRoomService/:${clienteId}`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(formData)
-          });
+      e.preventDefault();
 
-          if(!response.ok) {
-            throw new Error(`Error al enviar la solicitud: ${response.statusText}`);
-          }
-
-          const responseData = await response.json();
-          console.log('Datos enviado: ', responseData);
+      try {
+        const response = await fetch(`/enviarRoomService/${id}`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        });
+        if (!response.ok) {
+          throw new Error(`Error al enviar la solicitud: ${response.statusText}`);
+        }
+  
+        const responseData = await response.json();
+        console.log("Id RoomService: ", responseData);
         } catch (err) {
           console.error("Error al enviar la solicitud:", err);
         }
-        handleClear();
-    };
-
-    const handleClear = () => {
-        setFormData({
-            fecha: '',
-            hora: 0,
-            minutes: 0,
-            bebidas: '',
-            menu: '',
-            cantidadBebidas: 0,
-            cantidadMenu: 0,
-            otros_detalles: ''
-        });
     };
 
     const generateHoursOption = () => {
@@ -107,147 +93,294 @@ const BodyRoomServices = () => {
 
 
     return (
-      <div>
-        <div className="flex text-center justify-center mb-12">
-          <h1 className="text-7xl font-Abril-Fatface text-green-800 text-shadow-lg shadow-green-500/100 mb-4">
-            ROOMSERVICE
-          </h1>
-        </div>
-        <div className="flex justify-center items-center h-auto -mt-12 rounded-3xl">
-          <form
-            onSubmit={handleSubmit}
-            className="w-full max-w-md bg-lime-100 p-8 rounded-green-600 rounded-2xl shadow-md border border-black py-4 px-8"
+      <>
+      <div
+        className="
+      flex
+      text-center
+      justify-center
+      mt-4
+      
+      "
+      >
+        <h1
+          className="
+        text-7xl
+        font-Abril-Fatface
+        text-green-800
+        text-shadow-lg
+        shadow-green-500/100
+       
+        mb-1
+        "
+        >
+          ROOMSERVICE
+        </h1>
+      </div>
+      <div
+        className="
+      flex 
+      justify-center 
+      items-center 
+      h-screen
+      -mt-24
+      rounded-3xl
+      
+      "
+      >
+        <form
+          onSubmit={handleSubmit}
+          className="
+          w-full 
+          max-w-md 
+          bg-lime-100 
+          p-8
+          rounded-green-600 
+          rounded-2xl 
+          shadow-md
+          border
+        border-black"
+        >
+          <label
+            className="
+          block 
+          text-gray-700 
+          text-sm 
+          font-bold 
+          mb-2"
           >
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Fecha
-            </label>
-            <input
-              type="date"
-              name="fecha"
-              value={formData.fecha}
-              onChange={handleChange}
-              className="w-full border border-gray-300 p-2 mb-4"
-            />
-  
-            <div className="flex justify-between mb-4">
-              <div className="w-1/2 pr-2">
-                <label className="block text-gray-700 text-sm font-bold">
-                  Hora
-                </label>
-                <select
-                  name="hora"
-                  value={formData.hora}
-                  onChange={handleChange}
-                  className="w-full border border-gray-300 p-2"
-                >
-                  {generateHoursOption()}
-                </select>
-              </div>
-  
-              <div className="w-1/2 pl-2">
-                <label className="block text-gray-700 text-sm font-bold">
-                  Minutos
-                </label>
-                <select
-                  name="minutes"
-                  value={formData.minutes}
-                  onChange={handleChange}
-                  className="w-full border border-gray-300 p-2"
-                >
-                  {generateMinutesOption()}
-                </select>
-              </div>
+            Fecha
+          </label>
+          <input
+            type="date"
+            name="fecha"
+            value={formData.fecha}
+            onChange={handleChange}
+            className="
+            w-full border 
+            border-gray-300 
+            p-2 
+            mb-4"
+          />
+
+          <div
+            className="
+          flex 
+          justify-between 
+          mb-4
+          "
+          >
+            <div className="w-1/2 pr-2">
+              <label className="block  text-gray-700  text-sm  font-bold ">
+                Hora
+              </label>
+              <select
+                name="hora"
+                value={formData.hora}
+                onChange={handleChange}
+                className="
+                w-full border 
+                border-gray-300 
+                p-2
+                "
+              >
+                {generateHoursOption()}
+              </select>
             </div>
-  
-            <div className="flex justify-between mb-4">
-              <div className="w-1/2 pr-2">
-                <label className="block text-gray-700 text-sm font-bold mb-1">
-                  Bebidas
-                </label>
-                <select
-                  name="bebidas"
-                  value={formData.bebidas}
-                  onChange={handleChange}
-                  className="w-full border border-gray-300 p-2"
-                >
-                  <option value="">Seleccionar Bebida</option>
-                  {bebidasOptions.map((bebida) => (
+
+            <div
+              className="
+            w-1/2 
+            pl-2
+            "
+            >
+              <label
+                className="
+              block 
+              text-gray-700 
+              text-sm 
+              font-bold 
+              "
+              >
+                Minutos
+              </label>
+              <select
+                name="minutes"
+                value={formData.minutes}
+                onChange={handleChange}
+                className="
+                w-full 
+                border 
+                border-gray-300 
+                p-2"
+              >
+                {generateMinutesOption()}
+              </select>
+            </div>
+          </div>
+
+          <div
+            className="
+          flex 
+          justify-between 
+          mb-4"
+          >
+            <div
+              className="
+            w-1/2 
+            pr-2"
+            >
+              <label className=" block  text-gray-700  text-sm  font-bold  mb-1">
+                Bebidas
+              </label>
+              <select
+                name="bebidaOption"
+                value={formData.bebidaOption}
+                onChange={handleChange}
+                className="
+                w-full border 
+                border-gray-300 
+                p-2"
+              >
+                <option value="">Seleccionar Bebida</option>
+                {bebidasOptions &&
+                  bebidasOptions.map((bebida) => (
                     <option key={bebida.id} value={bebida.nombres}>
                       {bebida.nombres}
                     </option>
                   ))}
-                </select>
-              </div>
-  
-              <div className="w-1/2 pl-2">
-                <label className="block text-gray-700 text-sm font-bold mb-1">
-                  Cantidad de bebidas
-                </label>
-                <input
-                  type="number"
-                  name="cantidadBebidas"
-                  value={formData.cantidadBebidas}
-                  onChange={handleChange}
-                  className="w-full border border-gray-300 p-2"
-                />
-              </div>
+              </select>
             </div>
-  
-            <div className="flex justify-between">
-              <div className="w-1/2 pr-2">
-                <label className="block text-gray-700 text-sm font-bold">
-                  Menu
-                </label>
-                <select
-                  name="menu"
-                  value={formData.menu}
-                  onChange={handleChange}
-                  className="w-full border border-gray-300 p-2"
-                >
-                  <option value="">Seleccionar menu</option>
-                  {menuOptions.map((menu) => (
+
+            <div
+              className="
+            w-1/2 
+            pl-2"
+            >
+              <label
+                className="
+              block 
+              text-gray-700 
+              text-sm 
+              font-bold 
+              mb-1"
+              >
+                Cantidad de bebidas
+              </label>
+              <input
+                type="number"
+                name="cant_beb"
+                value={formData.cant_beb}
+                onChange={handleChange}
+                className="
+                w-full border 
+                border-gray-300 
+                p-2"
+              />
+            </div>
+          </div>
+
+          <div
+            className="
+          flex 
+          justify-between 
+          "
+          >
+            <div
+              className="
+            w-1/2 
+            pr-2"
+            >
+              <label
+                className="
+              block 
+              text-gray-700 
+              text-sm 
+              font-bold 
+              "
+              >
+                Menu
+              </label>
+              <select
+                name="menuOption"
+                value={formData.menuOption}
+                onChange={handleChange}
+                className="
+                w-full 
+                border 
+                border-gray-300 
+                p-2"
+              >
+                <option value="">Seleccionar menu</option>
+                {menuOptions &&
+                  menuOptions.map((menu) => (
                     <option key={menu.id} value={menu.nombres}>
                       {menu.nombres}
                     </option>
                   ))}
-                </select>
-              </div>
-  
-              <div className="w-1/2 pl-2">
-                <label className="block text-gray-700 text-sm font-bold">
-                  Cantidad de comida
-                </label>
-                <input
-                  type="number"
-                  name="cantidadMenu"
-                  value={formData.cantidadMenu}
-                  onChange={handleChange}
-                  className="w-full border border-gray-300 p-2"
-                />
-              </div>
+              </select>
             </div>
-  
-            <div className="mb-1">
-              <label className="block text-gray-700 text-sm font-bold mb-1">
-                Información adicional
+
+            <div
+              className="
+            w-1/2 
+            pl-2"
+            >
+              <label
+                className="
+              block 
+              text-gray-700 
+              text-sm 
+              font-bold 
+              "
+              >
+                Cantidad de comida
               </label>
-              <textarea
-                name="resumen"
-                value={formData.otros_detalles}
+              <input
+                type="number"
+                name="cant_menu"
+                value={formData.cant_menu}
                 onChange={handleChange}
-                className="w-full border border-gray-300 p-2"
+                className="
+                w-full 
+                border 
+                border-gray-300 
+                p-2"
               />
             </div>
-  
-            <button
-              type="submit"
-              className="flex ml-28 bg-green-600 hover:bg-green-800 text-white font-bold py-3 px-8 mr-2 rounded mt-2"
-            >
-              Enviar
-            </button>
-          </form>
-        </div>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-1">
+              Información adicional
+            </label>
+            <textarea
+              name="otros_detalles"
+              value={formData.otros_detalles}
+              onChange={handleChange}
+              className="w-full border border-gray-300 p-2"
+            />
+          </div>
+
+          {/* Resto del formulario ... */}
+
+          <button
+            className="
+            flex
+            ml-28
+            bg-green-600 
+            hover:bg-green-800
+            text-white font-bold 
+            py-3 
+            px-8 
+            mr-2 
+            rounded
+            mt-2"
+          >
+            Enviar
+          </button>
+        </form>
       </div>
+    </>
     );
   }
 
